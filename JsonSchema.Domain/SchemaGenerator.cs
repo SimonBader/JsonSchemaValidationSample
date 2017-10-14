d418ede575fbe97e51290892a3e35625cb0d388f
Simon Bader <simonbader@yahoo.com>
2017-10-14 09:48:24 +0200

    Code re-factoring.

---
+++ JsonSchema.Domain/SchemaGenerator.cs
@@ -0,0 +1,15 @@
+﻿using JsonSchema.Model;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Schema.Generation;
+
+namespace JsonSchema
+{
+    public class Validator
+    {
+        public JSchema GetPersonSchema()
+        {
+            var generator = new JSchemaGenerator();
+            return generator.Generate(typeof(Person));
+        }
+    }
+}
---
+++ JsonSchema.Tests/SchemaGeneratorTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using JsonSchema.Model;
+using Newtonsoft.Json.Schema;
+using System.Collections.Generic;
+
+namespace JsonSchema.Tests
+{
+    [TestClass]
+    public class SchemaGeneratorTests
+    {
+        [TestMethod]
+        public void ShouldGeneratePersonSchemaWithSuccessfulValidation()
+        {
+            Assert.IsTrue(ShouldGeneratePersonSchema("Hans"));
+        }
+
+        [TestMethod]
+        public void ShouldGeneratePersonSchemaWithFailedValidation()
+        {
+            Assert.IsFalse(ShouldGeneratePersonSchema("Annemaria-Helena"));
+        }
+
+        private bool ShouldGeneratePersonSchema(string firstName)
+        {
+            var sut = new Validator();
+            var schema = sut.GetPersonSchema();
+            var person = CreatePerson(firstName);
+            var json = JObject.FromObject(person);
+
+            return json.IsValid(schema, out IList<ValidationError> errors);
+        }
+
+        private Person CreatePerson(string firstName)
+        {
+            return new Person
+            {
+                FirstName = firstName,
+                LastName = "Muster",
+                Age = 30,
+                BillingAddress = new Address { Street = "Alexanderplatz", HouseNumber = 23, City = "Berlin" },
+                DeliveryAddress = new Address { Street = "Schanzenstrasse", HouseNumber = 11, City = "Hamburg" }
+            };
+        }
+    }
+}
